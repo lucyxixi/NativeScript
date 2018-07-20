7aa36a4a369fd8d0c791ce62bf58c802df64dc76
lucyxixi <xixi52372122@gmail.com>
2018-07-20 20:29:49 +0800

    Side Drawer

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 import { AppRoutingModule } from "./app.routing";
 import { AppComponent } from "./app.component";
+import { NativeScriptUISideDrawerModule } from "nativescript-telerik-ui/sidedrawer/angular";
 
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
@@ -17,6 +18,8 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
  import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
  
  import { baseURL } from './shared/baseurl';
+ import { DrawerComponent } from './shared/drawer/drawer.component';
+
 
 @NgModule({
     bootstrap: [
@@ -25,12 +28,14 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     imports: [
         NativeScriptModule,
         AppRoutingModule,
-        NativeScriptHttpModule
+        NativeScriptHttpModule,
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
         AppComponent,
         MenuComponent,
-        DishdetailComponent
+        DishdetailComponent,
+        DrawerComponent
     ],
     providers: [
         {provide: 'BaseURL', useValue: baseURL},
--- app/shared/baseurl.ts
+++ app/shared/baseurl.ts
@@ -1,3 +1,4 @@
 export const baseURL = "http://192.168.137.1:3000/";
 //export const baseURL = "http://192.168.1.5:3000/";
 //export const baseURL = "http://192.168.191.145:3000/";
+//export const baseURL = "http://172.16.100.49:3000/";
